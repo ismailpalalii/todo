940e6e436440159b653cb90841036128ba3abb80
Ismail Palali <ismail.palali.pp@gmail.com>
2022-02-12 22:32:09 +0300

    Add tableview items & fixes view controller

---
+++ todo/Scene/TaskList/TableViewCell/ItemTableCellViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ItemTableCellViewController.swift
+//  todo
+//
+//  Created by ismail palali on 12.02.2022.
+//
+
+import UIKit
+
+class ItemTableCellViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ todo/Scene/TaskList/ViewController/TaskListViewController.swift
@@ -0,0 +1,94 @@
+//
+//  TaskListViewController.swift
+//  todo
+//
+//  Created by ismail palali on 12.02.2022.
+//
+
+import UIKit
+
+protocol TaskListDisplayLogic: AnyObject {
+  func displayTasks(viewModel: TaskList.FetchTasks.ViewModel)
+  func displayNewTaskAddition(viewModel: TaskList.AddNewTask.ViewModel)
+  func displayTaskEditing(viewModel: TaskList.EditTask.ViewModel)
+  func displayTaskDeletion(viewModel: TaskList.DeleteTask.ViewModel)
+}
+
+final class TaskListViewController: UIViewController {
+  @IBOutlet private weak var tableView: UITableView!
+  
+  var interactor: TaskListBusinessLogic!
+  var router: TaskListRoutingLogic!
+  
+  private var tableViewDataSource: TaskListTableViewDataSource!
+  private var tableViewDelegate: TaskListTableViewDelegate!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    configureNavigationItem()
+    configureTableView()
+    fetchTasks()
+  }
+}
+
+// MARK: TaskListViewController Scoped Methods
+
+private extension TaskListViewController {
+  func configureNavigationItem() {
+    navigationItem.rightBarButtonItem = .init(barButtonSystemItem: .add, target: self, action: #selector(didTapAddButton))
+  }
+  
+  func configureTableView() {
+    tableView.register(ItemTableViewCell.nib, forCellReuseIdentifier: ItemTableViewCell.reuseIdentifier)
+    
+    tableViewDataSource = TaskListTableViewDataSource(cellDeletionHandler: deleteCell)
+    tableViewDelegate = TaskListTableViewDelegate(cellSelectionHandler: selectedCell)
+    
+    tableView.dataSource = tableViewDataSource
+    tableView.delegate = tableViewDelegate
+    
+    tableView.tableFooterView = UIView()
+  }
+  
+  func fetchTasks() {
+    let request = TaskList.FetchTasks.Request()
+    interactor.fetchTasks(request: request)
+  }
+  
+  @objc func didTapAddButton() {
+    let request = TaskList.AddNewTask.Request()
+    interactor.addNewTask(request: request)
+  }
+  
+  func selectedCell(at indexPath: IndexPath) {
+    let request = TaskList.EditTask.Request(index: indexPath.row)
+    interactor.editTask(request: request)
+  }
+  
+  func deleteCell(at indexPath: IndexPath) {
+    let request = TaskList.DeleteTask.Request(index: indexPath.row)
+    interactor.deleteTask(request: request)
+  }
+}
+
+// MARK: Display Logic
+
+extension TaskListViewController: TaskListDisplayLogic {
+  func displayTasks(viewModel: TaskList.FetchTasks.ViewModel) {
+    tableViewDataSource.cellViewModels = viewModel.cellViewModels
+    tableView.reloadData()
+  }
+  
+  func displayNewTaskAddition(viewModel: TaskList.AddNewTask.ViewModel) {
+    router.routeToTaskEditor(viewModel: viewModel)
+  }
+  
+  func displayTaskEditing(viewModel: TaskList.EditTask.ViewModel) {
+    router.routeToTaskEditorForEditing(viewModel: viewModel)
+  }
+  
+  func displayTaskDeletion(viewModel: TaskList.DeleteTask.ViewModel) {
+    tableViewDataSource.cellViewModels = viewModel.cellViewModels
+  }
+}
